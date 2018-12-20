8358c9baf933e16f228cc9fc716f876ca2249af9
zxs <123456789@qq.com>
2018-12-20 18:04:34 +0800

    vue

--- heima_blog/app.js
+++ heima_blog/app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 app.set('view engine','ejs')
 app.set('views','./views')
+app.use('/node_modules',express.static('./node_modules'))
 app.get('/',(req,res)=>{
     res.render('index.ejs',{name:'zss',age:'18'})
 })
